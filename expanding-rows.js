bd4c07b564e558c97343c249836593be0a329317
Alex Null <immut4ble@gmail.com>
2017-05-17 18:22:07 +1200

    first commit

---
+++ expanding-rows.js
@@ -0,0 +1,17 @@
+var x = function(row) {
+        var element = document.getElementsByClassName(row)[0];
+        if (element.classList.contains("expanded")) {
+                element.classList.add("contracted");
+                element.classList.remove("expanded");
+        } else {
+                element.classList.add("expanded");
+                element.classList.remove("contracted");
+        }
+}
+
+document.getElementById("rants").onclick = function() { x("rants") };
+document.getElementById("essays").onclick = function() { x("essays") };
+document.getElementById("comics").onclick = function() { x("comics") };
+document.getElementById("blogs").onclick = function() { x("blogs") };
+document.getElementById("talks").onclick = function() { x("talks") };
+document.getElementById("cheatsheets").onclick = function() { x("cheatsheets") };
